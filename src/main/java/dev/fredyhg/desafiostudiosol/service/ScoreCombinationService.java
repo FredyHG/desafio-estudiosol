804dc89f011479477623ef29f5630c7d154be9a2
Fredy <fredyultima2@gmail.com>
2024-06-09 11:42:24 -0300

    rename

--- src/main/java/dev/fredyhg/desafiostudiosol/service/ScoreCombinationService.java
+++ src/main/java/dev/fredyhg/desafiostudiosol/service/ScoreCombinationService.java
@@ -3,11 +3,10 @@ package dev.fredyhg.desafiostudiosol.service;
 import dev.fredyhg.desafiostudiosol.request.VerifyRequest;
 import dev.fredyhg.desafiostudiosol.response.VerifyResponse;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
-public interface MatchCombinationService {
+public interface ScoreCombinationService {
 
     VerifyResponse verifyCombinations(VerifyRequest verifyRequest);
 
