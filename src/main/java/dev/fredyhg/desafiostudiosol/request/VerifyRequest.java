bc4d5067ff06d8df4589053916cbf61d79ee5838
Fredy <fredyultima2@gmail.com>
2024-06-10 21:30:26 -0300

    change to record

--- src/main/java/dev/fredyhg/desafiostudiosol/request/VerifyRequest.java
+++ src/main/java/dev/fredyhg/desafiostudiosol/request/VerifyRequest.java
@@ -1,8 +1,4 @@
 package dev.fredyhg.desafiostudiosol.request;
 
-import lombok.Data;
-
-@Data
-public class VerifyRequest {
-    private String score;
+public record VerifyRequest(String score) {
 }
